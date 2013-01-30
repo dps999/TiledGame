cadc7b2944c47db34636604064826728a9058806
dps999 <drobasergey@gmail.com>
2013-01-30 17:50:02 +0400

    new

--- TiledGame/Classes/HelloWorldScene.cpp
+++ TiledGame/Classes/HelloWorldScene.cpp
@@ -2,6 +2,8 @@
 
 using namespace cocos2d;
 
+
+
 CCScene* HelloWorld::scene()
 {
     CCScene * scene = NULL;
@@ -15,8 +17,14 @@ CCScene* HelloWorld::scene()
         HelloWorld *layer = HelloWorld::create();
         CC_BREAK_IF(! layer);
 
-        // add layer as a child to scene
-        scene->addChild(layer);
+		HelloWorldHud* layerHud = HelloWorldHud::create();
+		CC_BREAK_IF(! layerHud);
+
+		scene->addChild(layer);
+
+		scene->addChild(layerHud);
+
+		layer->_hud = layerHud;
     } while (0);
 
     // return the scene
@@ -30,13 +38,14 @@ bool HelloWorld::init()
     bool bRet = false;
     do 
     {
-		
+		_numCollected=0;
+		label = CCLabelTTF::create("","Artial", 32);
         //////////////////////////////////////////////////////////////////////////
         // super init first
         //////////////////////////////////////////////////////////////////////////
 
         CC_BREAK_IF(! CCLayer::init());
-
+		this->setTouchEnabled(true);
         //////////////////////////////////////////////////////////////////////////
         // add your codes below...
         //////////////////////////////////////////////////////////////////////////
@@ -64,15 +73,22 @@ bool HelloWorld::init()
 
 		_tileMap = CCTMXTiledMap::create("TileGameResources/TileMap.tmx");
 		_background = _tileMap->layerNamed("Background");
+		_foreground = _tileMap->layerNamed("Foreground");
+		_meta = _tileMap->layerNamed("Meta");
+		_meta->setVisible(false);
 		this->addChild(_tileMap, 1);	
 
 		CCTMXObjectGroup *objects = _tileMap->objectGroupNamed("Objects");
-		CCAssert(objects != NULL, "'Objects' object group not found");
+		//CCAssert(objects != NULL, "'Objects' object group not found");
 	//	CCStringToStringDictionary* spawnPoint = objects->objectNamed("SpawnPoint");
 	//	CCAssert(spawnPoint != NULL, "Spawn Point not Found!");
 		CCDictionary* spawnPoint = objects->objectNamed("SpawnPoint");
-		int x = ((CCString*)spawnPoint->objectForKey("x"))->intValue();
-        int y = ((CCString*)spawnPoint->objectForKey("y"))->intValue();
+		float x = ((CCString*)spawnPoint->objectForKey("x"))->intValue();
+        float y = ((CCString*)spawnPoint->objectForKey("y"))->intValue();
+		_player = CCSprite::create("TileGameResources/Player.png");
+		_player->setPosition(ccp(x,y));
+		this->addChild( _player,2);
+		this->setViewpointCenter(_player->getPosition());
 
 
         // 2. Add a label shows "Hello World".
@@ -104,9 +120,96 @@ bool HelloWorld::init()
     return bRet;
 }
 
+void HelloWorld::setViewpointCenter(CCPoint position)
+{
+ 
+    CCSize winSize = CCDirector::sharedDirector()->getWinSize();
+ 
+    int x = MAX(position.x, winSize.width / 2);
+    int y = MAX(position.y, winSize.height / 2);
+	x = MIN(x, (_tileMap->getMapSize().width * _tileMap->getMapSize().width) 
+        - winSize.width / 2);
+    y = MIN(y, (_tileMap->getMapSize().height * _tileMap->getMapSize().height) 
+        - winSize.height/2);
+    CCPoint actualPosition = ccp(x, y);
+ 
+    CCPoint centerOfView = ccp(winSize.width/2, winSize.height/2);
+    CCPoint viewPoint = ccpSub(centerOfView, actualPosition);
+	this->setPosition(viewPoint);
+ 
+}
+
+void HelloWorld::ccTouchesEnded(cocos2d::CCSet* touches, cocos2d::CCEvent* event)
+{
+	CCTouch* touch = (CCTouch*)( touches->anyObject() );
+	CCPoint touchLocation = touch->locationInView();
+	touchLocation = CCDirector::sharedDirector()->convertToGL(touchLocation);
+	touchLocation = this->convertToNodeSpace(touchLocation);
+
+	CCPoint playerPos = _player->getPosition();
+    CCPoint diff = ccpSub(touchLocation, playerPos);
+    if (abs(diff.x) > abs(diff.y)) {
+        if (diff.x > 0) {
+			playerPos.x += _tileMap->getTileSize().width;
+        } else {
+            playerPos.x -= _tileMap->getTileSize().width; 
+        }    
+    } else {
+        if (diff.y > 0) {
+            playerPos.y += _tileMap->getTileSize().height;
+        } else {
+            playerPos.y -= _tileMap->getTileSize().height;
+        }
+    }
+ 
+	if (playerPos.x <= (_tileMap->getMapSize().width * _tileMap->getMapSize().width) &&
+        playerPos.y <= (_tileMap->getMapSize().height * _tileMap->getMapSize().height) &&
+        playerPos.y >= 0 &&
+        playerPos.x >= 0 ) 
+    {
+		setPlayerPosition(playerPos);
+    }
+ 
+	this->setViewpointCenter(playerPos);
+}
+
+CCPoint HelloWorld::tileCoordForPosition(CCPoint position)
+{
+	int x = position.x / _tileMap->getTileSize().width;
+	int y = ((_tileMap->getMapSize().height * _tileMap->getTileSize().height) - position.y) / _tileMap->getTileSize().height;
+    return ccp(x, y);
+}
+
+void HelloWorld::setPlayerPosition(CCPoint position)
+{
+	CCPoint tileCoord = this->tileCoordForPosition(position);
+	int tileGid = _meta->tileGIDAt(tileCoord);
+	if (tileGid) {
+		CCDictionary *properties = _tileMap->propertiesForGID(tileGid);
+		if (properties) {
+			CCString *collision = (CCString*)properties->valueForKey("Collidable");
+	        if (collision && collision->compare("True")>-1) {
+				return;
+			}
+			CCString *collectable  = (CCString*)properties->valueForKey("Collectable");
+	        if (collectable && collectable->compare("True")>-1) {
+				_numCollected++;
+				_hud->numCollectedChanged(_numCollected);
+				_meta->removeTileAt(tileCoord);
+				_foreground->removeTileAt(tileCoord);
+			}
+		}
+	}
+	_player->setPosition( position);
+}
+
+void HelloWorldHud::numCollectedChanged(int numCollected) {
+	const char *score = new char;
+    label->setString( "0");
+}
+
 void HelloWorld::menuCloseCallback(CCObject* pSender)
 {
     // "close" menu item clicked
     CCDirector::sharedDirector()->end();
 }
-
--- TiledGame/Classes/HelloWorldScene.h
+++ TiledGame/Classes/HelloWorldScene.h
@@ -5,15 +5,34 @@
 
 #include "Box2D/Box2D.h"
 
+class HelloWorldHud : public cocos2d::CCLayer
+{
+	public:
+		cocos2d::CCLabelTTF *label;
+		void numCollectedChanged(int numCollected);
+
+		CREATE_FUNC(HelloWorldHud);
+};
+
 class HelloWorld : public cocos2d::CCLayer
 {
-public:
 
+public:
     // Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
+	cocos2d::CCTMXLayer *_meta;
 	cocos2d::CCTMXTiledMap *_tileMap;
 	cocos2d::CCTMXLayer *_background;
 	cocos2d::CCSprite *_player;
+	cocos2d::CCTMXLayer *_foreground;
+	int _numCollected;
+	HelloWorldHud *_hud;
+
     virtual bool init();  
+	void setViewpointCenter(cocos2d::CCPoint position);
+	cocos2d::CCPoint tileCoordForPosition(cocos2d::CCPoint position);
+	void setPlayerPosition(cocos2d::CCPoint position);
+	//void registerWithTouchDispatcher();
+	void ccTouchesEnded(cocos2d::CCSet* touches, cocos2d::CCEvent* event);
 
     // there's no 'id' in cpp, so we recommand to return the exactly class pointer
     static cocos2d::CCScene* scene();
